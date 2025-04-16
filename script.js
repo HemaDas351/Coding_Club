a3be73b9d15e5f3ba8c18ebf6f779eef1fcd515d
hema <hemadas47@gmail.com>
2025-04-17 00:06:37 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+    const nav = document.getElementById("navLinks");
+    nav.classList.toggle("show");
+  }
+  
+// uncheck radio buttons (unfortunatley this cannot be done with CSS alone)
+
+document.querySelectorAll('input[type="radio"]').forEach(radio => {
+  radio.addEventListener('click', (e) => {
+      e.preventDefault();
+      // setting a timeout enables this hack to work
+      setTimeout(() => radio.checked = !radio.checked, 0);
+  });
+});
